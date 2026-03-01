182ed73f212c21902c8c026e83fbd4ce17afe3bc
Shubham277353 <golubhandari405@gmail.com>
2026-03-01 23:49:06 +0530

    solve simple problem to add , sum and find max of an array

--- src/hello.java
+++ src/hello.java
@@ -2,14 +2,27 @@ import  java.util.Scanner;
 public class hello {
 
     public static void main(String[] args) {
-        System.out.println("Enter numbers for an array : ");
-        Scanner sc = new Scanner(System.in);
-        int[] arr = new int[5];
-        for(int i = 0 ; i < 5; i++){
-            arr[i] = sc.nextInt();
+        int [] arr = new int[5];
+        System.out.println("Enter five numbers u wanna add : ");
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0 ; i <5;i++){
+            arr[i] = scan.nextInt();
         }
-        for (int j : arr) {
-            System.out.println(j * 2);
+        int sum = 0;
+        for ( int j : arr){
+            sum += j ;
         }
+        System.out.println("Sum of the array = " + sum);
+
+        int max = arr[0];
+
+        for ( int k : arr){
+            if( k > max){
+                max = k;
+            }
+        }
+        System.out.println("Largest number in the array is " + max);
+
+        scan.close();
     }
 }
