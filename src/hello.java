a1ff140a3b39c2f6406cdce94fd64a732b27ad63
Shubham277353 <golubhandari405@gmail.com>
2026-03-01 23:29:32 +0530

    understand arrays

--- src/hello.java
+++ src/hello.java
@@ -1,13 +1,15 @@
+import  java.util.Scanner;
 public class hello {
 
     public static void main(String[] args) {
-        int result = sum(10,20,20);
-        System.out.println(result);
-    }
-    public static int sum(int a , int b){
-        return  a + b;
-    }
-    public static int sum(int a , int b , int c){
-        return  a + b + c;
+        System.out.println("Enter numbers for an array : ");
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        for(int i = 0 ; i < 5; i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int j : arr) {
+            System.out.println(j * 2);
+        }
     }
 }
