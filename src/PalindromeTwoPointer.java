c0cea8eae32b483829f1dbcaa94088a5ec8d4d4b
Shubham277353 <golubhandari405@gmail.com>
2026-05-03 15:27:52 +0530

    solve palindrome using two pointer

---
+++ src/PalindromeTwoPointer.java
@@ -0,0 +1,27 @@
+public class PalindromeTwoPointer {
+    public static void main(String[] args) {
+        String str = "racercar";
+
+        // we wanna check if this is palindrome or not without creating a seperate reverseString.
+        int i = 0;
+        int j = str.length()-1;
+        boolean palindrome = true;
+
+        for(int a = 0 ; a < str.length();a++){
+            char x = Character.toLowerCase(str.charAt(i));
+            char y = Character.toLowerCase(str.charAt(j));
+            if(x != y){
+                palindrome = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+        if(palindrome){
+                System.out.println("Is a palindrome");
+        }
+        else {
+            System.out.println("Is not a palindrome");
+        }
+    }
+}
