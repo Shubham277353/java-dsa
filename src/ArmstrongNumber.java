801b2630e68777b5db392dab550c886ad4d9db30
Shubham277353 <golubhandari405@gmail.com>
2026-05-03 16:05:55 +0530

    bruteforce armstrong checker

--- src/ArmstrongNumber.java
+++ src/ArmstrongNumber.java
@@ -1,2 +1,30 @@
-public class ArmstrongNumber {
-}
+    public class ArmstrongNumber {
+        public static void main(String[] args) {
+            int armNum = 1513;
+            int temp = armNum;
+            int newNum = 0 ;
+            int digitsCounter = 0;
+            int sum = 0;
+
+            while(armNum != 0){
+                int num = armNum % 10;
+                newNum = (newNum + num) * 10;
+                armNum = armNum/10;
+                digitsCounter++;
+            }
+            System.out.println(newNum);
+
+            // newNum = 3510;
+            while(newNum != 0){
+                int num = newNum %10;
+                sum = sum + (int) Math.pow(num ,digitsCounter);
+                newNum = newNum / 10 ;
+            }
+            System.out.println(sum);
+            if(sum == temp){
+                System.out.println("Its an armstrong number.");
+            }else{
+                System.out.println("It's not an armstrong Number.");
+            }
+        }
+    }
