6613cf71fb9c3dbbcc9da18105c9cefd42f89f8b
Shubham277353 <golubhandari405@gmail.com>
2026-06-05 23:53:41 +0530

    solved Leetcode problem #1 TwoSum using HashMap in o(n) time

--- src/array/TwoSum.java
+++ src/array/TwoSum.java
@@ -1,32 +1,20 @@
 package array;
 
+import java.util.HashMap;
+
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
-        int sum = 0;
-        int[] indices = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            System.out.println("i = " + i);
-            for (int j = 1; j < nums.length; j++) {
-                if (sum != target) {
-                    sum = nums[i] + nums[j];
-                    System.out.println(sum);
-                }
-                if (sum == target) {
-                    indices[0] = i;
-                    indices[1] = j;
-                    return indices;
-                }
+            int key = target - nums[i];
+            if (map.containsKey(key)) {
+                return new int[]{i, map.get(key)};
+            } else {
+                map.put(nums[i], i);
             }
+
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {2, 7, 11, 15};
-        int target = 9;
-        int[] index = twoSum(arr, target);
-        System.out.println(index[0]);
-        System.out.println(index[1]);
     }
 }
